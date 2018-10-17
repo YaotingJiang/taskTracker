caeaf7b036c47029547027c5e80a4ee6147298ee
YaotingJiang <yaotingjiang@gmail.com>
2018-10-16 22:54:17 -0400

    commit

---
+++ lib/taskTracker/tasks/task.ex
@@ -0,0 +1,22 @@
+defmodule TaskTracker.Tasks.Task do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "tasks" do
+    field :completed, :boolean, default: false
+    field :desc, :string
+    field :time, :integer
+    field :title, :string
+    field :user_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(task, attrs) do
+    task
+    |> cast(attrs, [:title, :desc, :time, :completed])
+    |> validate_required([:title, :desc, :time, :completed])
+  end
+end
